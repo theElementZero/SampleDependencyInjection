d316902f93963b10016aafe54b888fe06a5a5173
s00_21 <sushantnaik21@gmail.com>
2022-06-16 19:32:50 +0530

    Add files via upload

---
+++ WpfSampleDi/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using WpfSampleDi.Service;
+
+namespace WpfSampleDi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public readonly ILogBase LogBase;
+
+        public static MainWindow AppWindow;
+
+        public MainWindow(ILogBase logBase)
+        {
+            AppWindow = this;
+            LogBase = logBase;
+
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ WpfSampleDi/UserControlExample.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using WpfSampleDi.Service;
+
+namespace WpfSampleDi
+{
+    /// <summary>
+    /// Interaction logic for UserControlExample.xaml
+    /// </summary>
+    public partial class UserControlExample : UserControl
+    {
+        private readonly ILogBase _logBase;
+
+        public UserControlExample()
+        {
+            InitializeComponent();
+
+            if (MainWindow.AppWindow?.LogBase != null)
+            {
+                _logBase = MainWindow.AppWindow.LogBase;
+            }
+        }
+
+        private void SetButton_Click(object sender, RoutedEventArgs e)
+        {
+            _logBase.Info($"User url visit request");
+
+            try
+            {
+                var uri = new Uri(this.UserSource.Text, UriKind.Absolute);
+                _logBase.Debug($"User try visiting {uri}");
+                this.WebFrame.Source = uri;
+            }
+            catch (Exception error)
+            {
+                _logBase.Error(error, error.Message);
+            }
+        }
+
+        private void WebFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+        }
+    }
+}
